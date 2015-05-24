f63e9a41f1c9e49d543b5714ca946324fa3e975d
Carlos David <himura88@gmail.com>
2015-05-24 00:19:42 -0500

    Automata generator now takes its input from the command line.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
+#include <sstream>
 #include "StreamGenerator.h"
 #include "RandomNFAGenerator.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[])
+{
 
 
-    StreamGenerator * myStream;
-    RandomNFAGenerator * automataGenerator;
+    StreamGenerator *myStream;
+    RandomNFAGenerator *automataGenerator;
     automataGenerator = new RandomNFAGenerator();
 
-    automataGenerator->generateUniformRandomNFAs(3,9,10000);
+    unsigned alphabet, states, numberOfNFAs;
+
+    std::istringstream sigma(argv[1]);
+    std::istringstream numStates(argv[2]);
+    std::istringstream numNFAs(argv[3]);
+
+    sigma >> alphabet;
+    numStates >> states;
+    numNFAs >> numberOfNFAs;
+
+    automataGenerator->generateUniformRandomNFAs(alphabet, states, numberOfNFAs);
     myStream = new StreamGenerator();
 
-    StreamGenerator * soo;
-    soo = new StreamGenerator(502);
-    dynamic_bitset<> randomStream = soo->generateBitStream();
+    //StreamGenerator *soo;
+    //soo = new StreamGenerator(502);
+   // dynamic_bitset<> randomStream = soo->generateBitStream();
     //std::cout << "randomStream:  " << randomStream << std::endl;
-    dynamic_bitset<> foo = myStream->generateBitStream((256));
+   // dynamic_bitset<> foo = myStream->generateBitStream((256));
     //std::cout << "foo:  " << foo << std::endl;
     return 0;
 }
